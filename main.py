2d803a646828e28264b529375a8e66d9c74e0572
Mateus Cunha <mateus.cunha@ccc.ufcg.edu.br>
2023-04-10 02:10:38 -0300

    alteracoes para multithreading - todas as etapas

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
+import mmap
+import csv
 import requests
 import json
 from bs4 import BeautifulSoup as bs
 import re
+from time import time
 from datetime import datetime
 from datetime import timedelta
 import wget
 import os
 from gazpacho import Soup
+from threading import Thread
+from queue import Queue
+from urllib.request import urlopen, Request
+
 
 MAIN_URL = "https://www.in.gov.br/leiturajornal?"
-start_date = "01/02/2022"
-end_date = "02/02/2022"
-BASE_JSON_ID_DIR = './json_files/by_id/'
-BASE_JSON_MESANO_DIR = './json_files/by_mesano/'
-BASE_PDF_DIR = './pdf_files/'
+start_date = "03/04/2022"
+end_date = "05/04/2022"
+BASE_CSV_DIR = './csv_files/'
+BASE_JSON_DIR = './json_files/'
 BASE_HTML_DIR = './html_files/'
 
 SECOES_DOU = ['do1', 'do2', 'do3']
+
+CSV_COLS = [
+    'pubName',
+    'urlTitle',
+    'numberPage',
+    'subTitulo',
+    'titulo',
+    'title',
+    'pubDate',
+    'content',
+    'editionNumber',
+    'hierarchyLevelSize',
+    'artType',
+    'pubOrder',
+    'hierarchyStr',
+    'hierarchyList',
+    'content_full'
+]
 
 
 def generate_dates(start=start_date, end=end_date):
@@ -28,98 +52,294 @@ def generate_dates(start=start_date, end=end_date):
     dates_set = set()
 
     for date in interval:
-        dates_set.add(date.strftime('%d-%m-%Y'))
+        dates_set.add(date.strftime('%d/%m/%Y'))
 
     date_list = list(dates_set)
     date_list.sort()
 
-    for date in date_list:
-        print()
-        splitted_date = date.split('-')
-        print(splitted_date[1]+'/'+splitted_date[0])
+    return date_list
+
+def get_process_tracking_file_from_action(action):
+    if action == 'diarios':
+        return './download_diarios.txt'
+    elif action == 'atos':
+        return './download_atos.txt'
+    elif action == 'csv':
+        return './processamento_csv.txt'
+    return ''
+
+def write_processed_date(date, secao, action):
+    file_path = get_process_tracking_file_from_action(action)
+    with open(file_path, 'a') as file:
+        file.write(f'{date}-{secao}\n')
+        file.close()
+
+def find_date_in_processed_file(date, secao, action):
+    file_path = get_process_tracking_file_from_action(action)
+    with open(file_path) as file:
+        s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
+        b = bytearray()
+        b.extend(map(ord, f'{date}-{secao}'))
+        if s.find(b) != -1:
+            return True
+        else:
+            return False
+
+def convert_atos_to_csv(date = '02-02-2022'):
+    CSV_FILE_PATH = BASE_CSV_DIR + date + '.csv'
+    csv_file = open(CSV_FILE_PATH, 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(CSV_COLS)
+
+    for jornal in SECOES_DOU:
+        if find_date_in_processed_file(date, jornal, 'csv'):
+            print(f'{date} - {jornal} - Já processado!')
+            csv_file.close()
+            return
+        
+        JSON_FILE_PATH = BASE_JSON_DIR + date + '-' + jornal + '.json'
+
+        parsed_json = {}
+
+        with open(JSON_FILE_PATH, 'r') as json_file:
+            json_content = json_file.read()
+            parsed_json = json.loads(json_content)
+            json_file.close()
+
+        atos = parsed_json["jsonArray"]
 
 
-def get_id_from_title(title_text=''):
-    splitted = title_text.split()
-    for text in splitted:
-        if re.search("^\d*$", text):
-            return text
+        total_atos = len(atos)
+
+        if total_atos == 0:
+            print(f'\t{date} - {jornal} - Sem publicações!')
+
+        for ato in atos:
+            content_full = get_html_from_pub_order(date, jornal, ato["pubOrder"])
+            ato["content_full"] = content_full
+            conteudo = convert_to_csv_row(ato)
+            csv_writer.writerow(conteudo)
+
+    csv_file.close()
+
+def get_html_from_pub_order(date, jornal, pub_order):
+    HTML_ATOS_DIR_PATH = BASE_HTML_DIR + 'atos/' + date + '-' + jornal + '/'
+    content = ''
+    with open(HTML_ATOS_DIR_PATH + pub_order + '.html') as file:
+        content = file.read()
+    return content
+
+def fetch_atos_from_json(date = '02-02-2022', jornal = 'do1', queue = Queue()):
+    if find_date_in_processed_file(date, jornal, 'atos'):
+        print(f'{date} - {jornal} - Já processado!')
+        return
+
+    JSON_FILE_PATH = BASE_JSON_DIR + date + '-' + jornal + '.json'
+    HTML_ATOS_DIR_PATH = BASE_HTML_DIR + 'atos/' + date + '-' + jornal + '/'
+
+    parsed_json = {}
+
+    with open(JSON_FILE_PATH, 'r') as json_file:
+        json_content = json_file.read()
+        parsed_json = json.loads(json_content)
+        json_file.close()
+
+    # print(parsed_json["jsonArray"])
+    atos = parsed_json["jsonArray"]
+
+    total_atos = len(atos)
+    atos_para_processamento = 0
+
+    if total_atos == 0:
+        print(f'\t{date} - {jornal} - Sem publicações!')
+
+    for ato in atos:
+        queue.put((ato, HTML_ATOS_DIR_PATH, f'{date} - {jornal} - {atos_para_processamento} de {total_atos}'))
+        atos_para_processamento += 1
+    
+
+def single_ato_to_file(ato, target_path):
+    content_full = str(fetch_ato_content(ato["urlTitle"]))
+    pubOrder = ato["pubOrder"]
+    target_file_path = target_path + pubOrder + '.html'
+    with open(target_file_path, 'w') as t_file:
+        t_file.write(content_full)
 
 
-def export_json_files(mesano, dic_json_content={}):
-    export_mesano_json(mesano, dic_json_content)
-    pub_ids = dic_json_content.keys()
-    for pub_id in pub_ids:
-        export_id_json(pub_id, dic_json_content[pub_id])
+
+def convert_to_csv_row(conteudo):
+    row_list = []
+    for col in CSV_COLS:
+        if col in conteudo:
+            row_list.append(conteudo[col])
+        else:
+            row_list.append("")
+    return row_list
 
 
-def export_mesano_json(mesano, json_content):
-    replaced_mes_ano = mesano.replace('/', '-')
-    with open(BASE_JSON_MESANO_DIR + replaced_mes_ano + '.json', 'w') as outfile:
-        json.dump(json_content, outfile)
+def fetch_ato_content(url_title):
+    BASE_ATO_URL = "https://www.in.gov.br/web/dou/-/"
+    response_html = requests.get(f'{BASE_ATO_URL}{url_title}').text
+    response_html.replace(os.linesep, '')
+    soup = bs(response_html, 'html.parser')
+    elements = soup.find_all(attrs={"class": "texto-dou"})
+    if len(elements) == 1:
+        return elements[0]
+    else:
+        print('Erro ao obter ato de URL ' + url_title)
+        return ""
+
+def fetch_all_pubs_dia(date, DEST_PATH = "", secao="do1"):
+    if find_date_in_processed_file(date, secao, 'diarios'):
+        print(f'{date} - {secao} - Já processado!')
+        return
+
+    URL_STR = f'{MAIN_URL}data={date}&secao={secao}'
+    JSON_PATH = DEST_PATH.replace('html', 'json') + '.json'
+    with urlopen(URL_STR) as base_html, open(DEST_PATH, 'wb') as f, open(JSON_PATH, 'w') as f_json:
+        f.write(base_html.read())
+
+    with open(DEST_PATH, 'r') as o_file, open(JSON_PATH, 'w') as f_json:
+        content = o_file.read()
+        split1 = content.split('<script id="params" type="application/json">')
+        split2 = split1[1].split('</script>')
+        f_json.write(split2[0])
 
 
-def export_id_json(id, json_content):
-    with open(BASE_JSON_ID_DIR + id + '.json', 'w') as outfile:
-        json.dump(json_content, outfile)
+    create_atos_dir(date, secao)
+
+def create_atos_dir(date, jornal):
+    path = get_html_atos_files_path(date, jornal)
+    exists = os.path.exists(path)
+    if not exists:
+        os.makedirs(path)
+
+def get_html_atos_files_path(date, jornal):
+    return f'./html_files/atos/{date}-{jornal}'
+
+class DonwloadWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # diarios
+            # DATE_LINE_SEP, FILE_PATH, secao = self.queue.get()
+
+            # atos
+            # ato, HTML_ATOS_DIR_PATH, message_prefix = self.queue.get()
+
+            # csv
+            DATE_LINE_SEP = self.queue.get()
+            try:
+                # diarios
+                # print(f'{DATE_LINE_SEP} - {secao} -->> Iniciado!')
+                # fetch_all_pubs_dia(DATE_LINE_SEP, FILE_PATH, secao)
+                # print(f'{DATE_LINE_SEP} - {secao} <<-- Download concluído!')
+
+                # atos
+                # print(f'{message_prefix} -->> Iniciado!')
+                # single_ato_to_file(ato, HTML_ATOS_DIR_PATH)
+                # print(f'{message_prefix} <<-- Download concluído!')
+
+                # csv
+                print(f'{DATE_LINE_SEP} -->> Iniciado!')
+                convert_atos_to_csv(DATE_LINE_SEP)
+                print(f'{DATE_LINE_SEP} <<-- Download concluído!')
+            except:
+                with open('./error.txt', 'a') as error_file:
+                    # error_file.write(f'Erro! - {DATE_LINE_SEP} - {secao}')
+                    # error_file.write(f'Erro! - {message_prefix}\n\n')
+                    error_file.write(f'Erro! - {DATE_LINE_SEP}')
+            finally:
+                self.queue.task_done()
 
 
-def download_pdf_file(file_metadata):
-    splitted_mesano = file_metadata["mesano"].split('/')
-    mes = splitted_mesano[0]
-    ano = splitted_mesano[1]
+def main_diarios():
+    ts = time()
+    dates_list = generate_dates()
+    # dates_list = ["02/02/2022", "03/02/2022"]
 
-    if not os.path.exists(BASE_PDF_DIR + ano):
-        os.mkdir(BASE_PDF_DIR + ano)
+    queue = Queue()
 
-    if not os.path.exists(BASE_PDF_DIR + ano + '/' + mes):
-        os.mkdir(BASE_PDF_DIR + ano + '/' + mes)
+    for x in range(300):
+        worker = DonwloadWorker(queue)
+        worker.daemon = True
+        worker.start()
 
-    full_working_dir = BASE_PDF_DIR + ano + '/' + mes + '/'
+    for date in dates_list:
+        for secao in SECOES_DOU:
+            DATE_LINE_SEP = date.replace("/","-")
+            FILE_PATH = BASE_HTML_DIR + f'{DATE_LINE_SEP}-{secao}'
 
-    wget.download(file_url, full_working_dir + file_metadata["file_name"])
+            queue.put((DATE_LINE_SEP, FILE_PATH, secao))
+    
+    queue.join()
 
+    for date in dates_list:
+            DATE_LINE_SEP = date.replace("/","-")
+            for secao in SECOES_DOU:
+                write_processed_date(DATE_LINE_SEP, secao, 'diarios')
 
-# dates_list = generate_dates()
-dates_list = ["02/02/2022"]
+    print('Processamento concluído!')
+    print('Finalizado em %s', time() - ts)
 
-for date in dates_list:
-    data_by_id = {}
+def main_atos():
+    ts = time()
+    dates_list = generate_dates()
+    # dates_list = ["02/02/2022", "03/02/2022"]
 
-    response_html = requests.get(f'{MAIN_URL}data={date}&secao=do1').text
-    # response_html.replace('\n', '').replace('\t', '')
+    queue = Queue()
 
-    soup = Soup(response_html)
-    string = soup.find("script", {"id": "params"})
+    for x in range(500):
+        worker = DonwloadWorker(queue)
+        worker.daemon = True
+        worker.start()
 
-    print(string)
+    for date in dates_list:
+        for secao in SECOES_DOU:
+            DATE_LINE_SEP = date.replace("/","-")
+            fetch_atos_from_json(DATE_LINE_SEP, secao, queue)
 
-    # soup = bs(response_html, 'html.parser')
+            
+    queue.join()
 
-    # for a in soup.find("script"):
-        # print(a)
-    #     file_url = a["href"]
-    #     file_original_title = a.text.strip()
-    #     id = get_id_from_title(file_original_title)
+    for date in dates_list:
+            DATE_LINE_SEP = date.replace("/","-")
+            for secao in SECOES_DOU:
+                write_processed_date(DATE_LINE_SEP, secao, 'atos')
+                
+    print('Processamento concluído!')
+    print('Finalizado em %s', time() - ts)
 
-    #     print(a["href"])
-    #     print(a.text.strip())
-    #     dic = {
-    #         "id": id,
-    #         "file_url": file_url,
-    #         "file_original_title": file_original_title,
-    #         "date": date
-    #     }
+def main_csv():
+    ts = time()
+    dates_list = generate_dates()
+    # dates_list = ["02/02/2022", "03/02/2022"]
 
-    #     if id not in data_by_id.keys():
-    #         data_by_id[id] = []
-    #     else:
-    #         dic["id"] = file_original_title.split()[-1]
+    queue = Queue()
 
-    #     dic["file_name"] = dic["id"] + '.pdf'
-    #     data_by_id[id].append(dic)
+    for x in range(100):
+        worker = DonwloadWorker(queue)
+        worker.daemon = True
+        worker.start()
 
-    #     download_pdf_file(dic)
+    for date in dates_list:
+        DATE_LINE_SEP = date.replace("/","-")
+        queue.put((DATE_LINE_SEP))
+            
+    queue.join()
 
-    # print(data_by_id)
-    # export_json_files(date, data_by_id)
+    for date in dates_list:
+            DATE_LINE_SEP = date.replace("/","-")
+            for secao in SECOES_DOU:
+                write_processed_date(DATE_LINE_SEP, secao, 'csv')
+                
+    print('Processamento concluído!')
+    print('Finalizado em %s', time() - ts)
+
+if __name__ == '__main__':   
+    # main_diarios()
+    # main_atos()
+    main_csv()
